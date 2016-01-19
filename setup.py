377f2ee84e61c6b4dc072244f6fd770ab39cf780
Vincent Bastos <vincent@lavalab.com.au>
2016-01-19 13:26:37 +1000

    removed unecessary module requirement

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
 
-requires = ['isonasacs' >= 0.4]
+requires = []
 
 MODULE2PREFIX = {}
 
